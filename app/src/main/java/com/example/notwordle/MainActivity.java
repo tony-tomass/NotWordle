5e6ec3c6a584772a0c3c52075e10fdc453e326f3
tony-tomass <tonynguyen984@gmail.com>
2023-11-04 12:32:39 -0500

    Colors change ALMOST correctly based on the answer

--- app/src/main/java/com/example/notwordle/MainActivity.java
+++ app/src/main/java/com/example/notwordle/MainActivity.java
@@ -15,22 +15,27 @@ public class MainActivity extends AppCompatActivity {
     Button clear_bt;
     GridLayout grid;
     char[] word;
-    char[] answer;
+    String answer;
     int[][] box_state;
     static final int ROW_SIZE = 5;
     static final int WIN_CONDITION = 10;
+    int current_row_index;
 
     View.OnClickListener submit_listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            //For testing
+            /*
+            //For testing color
             box_state[0][0] = -2;
             box_state[0][1] = -1;
             box_state[0][2] = 0;
             box_state[0][3] = 1;
             box_state[0][4] = 2;
+             */
 
-            checkRows();
+            checkWord();
+            current_row_index++;
+            toNextRow();
         }
     };
     View.OnClickListener restart_listener = new View.OnClickListener() {
@@ -61,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         clear_bt.setOnClickListener(clear_listener);
 
         //TODO: Implement database to pull answers from
-        answer = new char[]{'B', 'R', 'A', 'I', 'N'};
+        answer = "brain";
 
         /*
         -2 = next row (greyed out)
@@ -71,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         2 = correct
         Starting off, the top row must all be 0 and all rows below -2
          */
+        current_row_index = 0;
         box_state = new int[][]{
                 {0, 0, 0, 0, 0},
                 {-2, -2, -2, -2, -2},
@@ -87,48 +93,115 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < grid.getChildCount(); i++) {
             EditText et = (EditText) grid.getChildAt(i);
 
-            Log.i("child_count", String.valueOf(grid.getChildCount()));
+            //Log.i("child_count", String.valueOf(grid.getChildCount()));
 
             //Gets EditText box's row and column numbers; explanations are there bc I'm slow
             int row = i / ROW_SIZE; //Int rounds down => First row = 0 and so on
             int col = i % ROW_SIZE; //0 % 5 = 0, 1 % 5 = 1....5 % 5 = 0, 6 % 5 = 0 and so on
 
-            Log.i("starting", "This is Row " + row + " Col. " + col);
+            //Log.i("starting", "This is Row " + row + " Col. " + col);
             switch (box_state[row][col]) {
                 case -2:
                     //TODO: Technically not needed since it won't be changing to this state ever, only starting at it
                     et.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.not_yet_et));
                     et.setTextColor(getResources().getColor(R.color.white));
                     et.setEnabled(false);
-                    Log.i("unselected", "This box is now grey-out and not selectable");
+                    //Log.i("unselected", "This box is now grey-out and not selectable");
                     break;
                 case -1:
                     et.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.incorrect_et));
                     et.setTextColor(getResources().getColor(R.color.white));
                     et.setEnabled(false);
-                    Log.i("incorrect", "This box is now dark gray and incorrect");
+                    //Log.i("incorrect", "This box is now dark gray and incorrect");
                     break;
                 case 0:
                     et.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.current_et));
                     et.setTextColor(getResources().getColor(R.color.black));
                     et.setEnabled(true);
-                    Log.i("current", "This box is now part of the current row and is selectable");
+                    //Log.i("current", "This box is now part of the current row and is selectable");
                     break;
                 case 1:
                     et.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.almost_correct_et));
                     et.setTextColor(getResources().getColor(R.color.white));
                     et.setEnabled(false);
-                    Log.i("almost", "This box is now yellow and almost correct");
+                    //Log.i("almost", "This box is now yellow and almost correct");
                     break;
                 case 2:
                     et.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.correct_et));
                     et.setTextColor(getResources().getColor(R.color.white));
                     et.setEnabled(false);
-                    Log.i("correct", "This box is now green and correct!");
+                    //Log.i("correct", "This box is now green and correct!");
                     break;
             }
         }
     }
 
+    public void toNextRow() {
+        Log.i("setting_next_row", "Setting up to next row...");
+
+        for (int i = 0; i < box_state[current_row_index].length; i++) {
+
+            box_state[current_row_index][i] = 0;
+
+            Log.i("boxes_done", "Box #" + i + " done.");
+
+        }
+        checkRows();
+    }
+
+    public void checkWord() {
+        StringBuilder stringBuilder = new StringBuilder();
+        String input_word = "";
+        Log.i("current_index_check", String.valueOf(current_row_index));
+
+        //(Current row index * (Length of row - 1) + i
+        //(0 * 4) + 0 = 0 => B
+        //(0 * 4) + 1 = 1 => R
+        //...
+        //(1 + 4) + 0 = 5
+
+        for (int i = 0; i < box_state[current_row_index].length; i++) {
+            EditText et = (EditText) grid.getChildAt(getChildAtCalculator(i));
+
+            Log.i("int_i_check", String.valueOf(i));
+            Log.i("current_letter", et.getText().toString());
+
+            stringBuilder.append(et.getText().toString());
+        }
+        input_word = stringBuilder.toString().toLowerCase();
+        Log.i("input_word", input_word);
+
+        for (int i = 0; i < box_state[current_row_index].length; i++) {
+            EditText et = (EditText) grid.getChildAt(getChildAtCalculator(i));
+            String box_letter = et.getText().toString().toLowerCase();
+
+            Log.i("box_letter", box_letter);
+            Log.i("check_if_contain", String.valueOf(answer.contains(box_letter)));
+
+            if (answer.contains(box_letter)) {
+
+                Log.i("index_of_letter", "Index of Input Letter '" + box_letter + "': " + input_word.indexOf(box_letter));
+                Log.i("index_of_ans_letter", "Index of Answer Letter '" + box_letter + "': " + answer.indexOf(box_letter));
+
+                if (input_word.indexOf(box_letter) == answer.indexOf(box_letter)) {
+                    //Correct
+                    box_state[current_row_index][i] = 2;
+                }
+                else {
+                    //Almost Correct
+                    box_state[current_row_index][i] = 1;
+                }
+            }
+            else {
+                //Incorrect
+                box_state[current_row_index][i] = -1;
+            }
+        }
+
+    }
+
+    public int getChildAtCalculator(int i) {
+        return (current_row_index * (box_state[current_row_index].length)) + i;
+    }
 
 }
